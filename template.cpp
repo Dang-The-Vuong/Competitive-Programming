db8ec728735daebad3de0e7ff6e3cb1605b5724f
Vuong <79716792+Dang-The-Vuong@users.noreply.github.com>
2021-07-18 02:59:47 +0000

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long  /* Shorten the code by turning long long into ll inspire by fishy15 */
+#define ld long double /* Shorten the code by turning long double into ld inspire by fishy15 */
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0); /* Make the code faster taken from fishy15 template. */
+  
+  return 0;
+}
+
+/* Template inspire by fishy15! https://github.com/fishy15 */
