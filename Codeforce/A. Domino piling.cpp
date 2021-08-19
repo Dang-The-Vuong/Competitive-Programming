f5e90685b3e06169ca69af59bc7a9b47a92aafbd
Vuong <79716792+Dang-The-Vuong@users.noreply.github.com>
2021-08-19 03:24:39 +0000

    Create A. Domino piling.cpp

---
+++ Codeforce/A. Domino piling.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  int m, n;
+  cin >> m >> n;
+  cout << (m*n)/2 << "\n";
+  return 0;
+}
