2bd7843f2df06545672a63cd378ac451144e3759
Vuong <79716792+Dang-The-Vuong@users.noreply.github.com>
2021-08-25 11:47:55 +0000

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,13 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long  /* Shorten the code by turning long long into ll inspire by fishy15 */
-#define ld long double /* Shorten the code by turning long double into ld inspire by fishy15 */
+#define ll long long
+#define ld long double
 
 int main() {
-  cin.tie(0)->sync_with_stdio(0); /* Make the code faster taken from fishy15 template. */
+  cin.tie(0)->sync_with_stdio(0);
   
   return 0;
 }
-
+/* Shorten the code by turning long long into ll inspire by fishy15 */
+/* Shorten the code by turning long double into ld inspire by fishy15 */
+/* Make the code faster taken from fishy15 template. */
 /* Template inspire by fishy15! https://github.com/fishy15/competitive_programming/blob/master/template.cpp */
